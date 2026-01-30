cc693c7f4535c30dfe98c4b06d3c356353834d4e
dargem <genericz145@gmail.com>
2026-01-30 20:14:46 +1100

    bench runner additions

--- src/benchmarks/bench_runner.hpp
+++ src/benchmarks/bench_runner.hpp
@@ -1,13 +1,72 @@
 #pragma once
 
 #include <vector>
+#include <stdexcept>
+#include <math.h>
 #include "src/benchmarks/benchable.hpp"
-
 namespace benchmarks {
 
-struct Benchmark {
-    
+struct Evaluation {
+    Evaluation(double meanCycles, double standardDeviation, double numTests)
+        : meanCycles{ meanCycles }, standardDeviation{ standardDeviation }, numTests{ numTests }
+    {}
+
+    // sampled mean number of cycles it took to do an iteration of the bench
+    const double meanCycles;
+
+    // sampled deviation of the averages of each bench
+    const double standardDeviation;
+
+    // number of benches this was built from
+    const double numTests;
 };
+
+struct Benchmark {
+    Benchmark(std::string_view name)
+        : name{ name }
+    {}
+
+    Evaluation createEvaluation() {
+
+        size_t numTests{ clockTimes.size() };
+
+        if (numTests < 30) {
+            std::runtime_error("Not enough tests run for central limit theorem");
+        }
+
+        double totalCycles{};
+        for (size_t clockTime : clockTimes) {
+            totalCycles += clockTime;
+        }
+        double sampleMean{ totalCycles / numTests };
+
+        double variance{};
+        for (size_t clockTime : clockTimes) {
+            variance += std::pow(clockTime - sampleMean, 2);
+        }
+
+        double standardDeviation{ std::pow(variance/(numTests - 1), 0.5) };
+        return Evaluation(sampleMean, standardDeviation, numTests);
+    }
+
+    const std::string_view name;
+    std::vector<const size_t> clockTimes;
+};
+
+class BenchmarkCollection {
+public:
+    // print the results of the benchmark out
+    void print() {
+
+    }
+
+    
+
+private:
+    std::vector<Benchmark> benchmarks;
+};
+
+
 
 // class for running benchmarks
 // also a singleton because I like singletons and its tightly linked to the timer singleton
@@ -26,8 +85,13 @@ public:
     void operator=(BenchRunner&&) = delete; // no move assignment
 
     // Move ownership of the benchable to the bench runner
-    void addBenchable(Benchable&& benchable) {
-        benchables.push_back(benchable);
+    void addBenchable(Benchable&& newBenchable) {
+        for (Benchable& benchable : benchables) {
+            if (typeid(benchable) == typeid(newBenchable)) {
+                throw std::runtime_error("Shouldn't have two of the same benchables");
+            }
+        }
+        benchables.push_back(newBenchable);
     }
 
     void clearBenchables() {
