b8d57396d92df5723fe93a203e7a0ab0ef18d127
dargem <genericz145@gmail.com>
2026-02-08 11:55:30 +1100

    use c++20, fix intellisense issue, start on confidence intervals for an evaluation

--- src/benchmarks/bench_runner.hpp
+++ src/benchmarks/bench_runner.hpp
@@ -3,13 +3,24 @@
 #include <vector>
 #include <stdexcept>
 #include <math.h>
+#include <iostream>
+#include <format>
 #include "src/benchmarks/benchable.hpp"
 namespace benchmarks {
 
+constexpr static size_t NUMBER_CLT_TESTS_NEEDED{ 30 };
+
 struct Evaluation {
-    Evaluation(double meanCycles, double standardDeviation, double numTests)
-        : meanCycles{ meanCycles }, standardDeviation{ standardDeviation }, numTests{ numTests }
-    {}
+    Evaluation(std::string_view name, double meanCycles, double standardDeviation, size_t numTests)
+        : name{ name }, meanCycles{ meanCycles }, standardDeviation{ standardDeviation }, numTests{ numTests }
+    {
+        // calculate the confidence interval
+        if (numTests < NUMBER_CLT_TESTS_NEEDED) {
+            throw new std::runtime_error(std::format("More tests need for central limit theorem, needed {} has {}", NUMBER_CLT_TESTS_NEEDED, numTests));
+        }
+    }
+
+    const std::string_view name;
 
     // sampled mean number of cycles it took to do an iteration of the bench
     const double meanCycles;
@@ -18,19 +29,26 @@ struct Evaluation {
     const double standardDeviation;
 
     // number of benches this was built from
-    const double numTests;
+    const size_t numTests;
+
+    // confidence interval made using CONFIDENCE_ALPHA global variable
+    constexpr static double CONFIDENCE_ALPHA{ 0.05 };
+    const double lowerConfidenceInterval;
+    const double upperConfidenceInterval;
 };
 
 struct Benchmark {
-    Benchmark(std::string_view name)
+    Benchmark(std::string_view name, size_t clockTime)
         : name{ name }
-    {}
+    {
+        clockTimes.push_back(clockTime);
+    }
 
     Evaluation createEvaluation() {
 
         size_t numTests{ clockTimes.size() };
 
-        if (numTests < 30) {
+        if (numTests < NUMBER_CLT_TESTS_NEEDED) {
             std::runtime_error("Not enough tests run for central limit theorem");
         }
 
@@ -46,24 +64,44 @@ struct Benchmark {
         }
 
         double standardDeviation{ std::pow(variance/(numTests - 1), 0.5) };
-        return Evaluation(sampleMean, standardDeviation, numTests);
+        return Evaluation(name, sampleMean, standardDeviation, numTests);
     }
 
+    
     const std::string_view name;
     std::vector<const size_t> clockTimes;
 };
 
 class BenchmarkCollection {
 public:
+    void addTime(std::string_view name, size_t timeTaken) {
+        for (Benchmark bench : benchmarks) {
+            if (bench.name == name) {
+                bench.clockTimes.push_back(timeTaken);
+                return;
+            }
+        }
+
+        // if its not in
+        benchmarks.push_back({name, timeTaken});
+    }    
+
     // print the results of the benchmark out
     void print() {
+        std::vector<Evaluation> evaluations;
+        evaluations.reserve(benchmarks.size());
 
+        for (Benchmark bench : benchmarks) {
+            evaluations.push_back(bench.createEvaluation());
+        }
     }
-
-    
 
 private:
     std::vector<Benchmark> benchmarks;
+
+    void printEvaluation(Evaluation evaluation) {
+
+    }
 };
 
 
