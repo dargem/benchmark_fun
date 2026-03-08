85071bed7216ef1461f1df7f6913642d1e93d74a
dargem <genericz145@gmail.com>
2026-03-09 10:51:08 +1100

    basic one way anova kinda

--- src/stats/anovas.hpp
+++ src/stats/anovas.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <ranges>
 #include <vector>
 
 #include "utils/concepts.hpp"
@@ -7,37 +8,57 @@
 // because we all love ANOVA don't we
 namespace stats {
 
-// used to aggregate some of the treatment stats together
-template <utils::Numeric T>
-struct TreatmentInfo {
-    // clang-format off
-    TreatmentInfo(const std::vector<T>& treatment) :
-            observations{treatment},
-            n{treatment.size()},
-            mean{std::accumulate(treatment.begin(), treatment.end(), 0.0,
-                                 [](double total, T y) { return total + y; }) / n}
-    {}
-    // clang-format on
-
-    const std::vector<T>& observations;
-    size_t n{};     // number of observations
-    double mean{};  // mean observations
-};
-
 // Performs a standard ANOVA to check if all treatments have the same mean
 // Returns true if null is accepted (all same mean), false if null is rejected (one different
 // mean)
 template <utils::Numeric T>
-bool sameGroupMeans(std::vector<std::vector<T>> treatments) {
+bool sameGroupMeans(std::vector<std::vector<T>>& treatments) {
     constexpr static double ALPHA = 0.05;
     // H0: u1 = u2 = u3 = etc all same means
     // HA: at least one treatment mean differs
 
-    // First get within group (error deviation) and across group (treatment deviation) variation
+    // First get within group (error deviation) and across group (treatment deviation) mean squares.
+    // Sum of Squares Within is = for each observation in each group, find its deviation from its
+    // own group mean. Then sum it all together. Its degrees of freedom is number of groups - 1. To
+    // get mean square of within divide SSW by its degrees of freedom.
+    double SSW{};
+    for (const std::vector<T>& treatment : treatments) {
+        double treatmentMean =
+            std::accumulate(treatment.begin(), treatment.end(), 0.0,
+                            [](double rollingSum, T val) { return rollingSum + val; }) /
+            treatment.size();
 
-    double grand_mean = grand_sum / grand_n;
+        for (T observation : treatment) {
+            SSW += std::pow(observation - treatmentMean, 2);
+        }
+    }
+    size_t withinDF = treatments.size() - 1;
+    double MSW = SSW / withinDF;  // Now have the mean square within
 
-    // The test statistic is an F-Ratio
-};
+    // To get the mean squares across/between, for each group sum the deviation of that groups mean
+    // from the grand mean. It has degrees of freedom of n - k, where n is total samples in
+    // population and k is number of groups. MSB (mean square between) = SSB / DF.
+
+    auto observations = std::views::all(treatments) | std::views::join;
+    double grandMean =
+        std::accumulate(observations.begin(), observations.end(), 0.0,
+                        [](double sum, T observation) { return sum + observation; }) /
+        observations.size();
+
+    double SSB{};
+    for (const std::vector<T>& treatment : treatments) {
+        double treatmentMean =
+            std::accumulate(treatment.begin(), treatment.end(), 0.0,
+                            [](double sum, T observation) { return sum + observation; }) /
+            treatment.size();
+        SSB += std::pow(treatmentMean - grandMean, 2);
+    }
+    size_t betweenDF = observations.size() - treatments.size();
+    double MSB = SSB / betweenDF;
+
+    // The test statistic is an F statistic, where F = MSB / MSW. If F > 1, this means more error is
+    // attributed to variation across groups than within groups.
+    double F = MSB / MSW;
+}
 
 }  // namespace stats
