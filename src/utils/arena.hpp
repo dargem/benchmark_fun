fa84a1085db0d8d88b2421c0824f2db15ff0f239
dargem <genericz145@gmail.com>
2026-05-01 23:12:35 +1000

    maybe arena works

--- src/utils/arena.hpp
+++ src/utils/arena.hpp
@@ -1,10 +1,15 @@
+#include <concepts>
 #include <cstddef>
+#include <cstdint>
+#include <cstring>
+#include <type_traits>
 
 namespace utils {
 
+template <bool Aligned>
 class Arena {
    public:
-    Arena() : data(new std::byte[BYTE_SIZE]) {}
+    Arena() : begin(new std::byte[CAPACITY]), end(begin + CAPACITY), current(begin) {}
 
     // delete copy construction / assignment
     Arena(const Arena&) = delete;
@@ -14,11 +19,33 @@ class Arena {
     Arena(Arena&&) = delete;
     Arena& operator=(Arena&&) = delete;
 
-    ~Arena() { delete[] data; }
+    ~Arena() { delete[] begin; }
+
+    template <typename T>
+        requires std::same_as<std::remove_cvref_t<T>, T>
+    T* allocate() {
+        if constexpr (Aligned) {
+            // we need to check we are adding stuff with proper alignment
+            // branchless increment to an aligned address
+            current += (reinterpret_cast<uintptr_t>(current) % alignof(T));
+            T* ptr = current;
+            current += sizeof(T);
+            return ptr;
+        } else {
+            T* ptr = current;
+            current += sizeof(T);
+            return ptr;
+        }
+    }
+
+    void reset() { current = begin; }
 
    private:
-    static constexpr size_t BYTE_SIZE = 100000;
-    std::byte* data;
+    static constexpr size_t CAPACITY = 1000000;  // Capacity in bytes
+
+    std::byte* begin;
+    std::byte* end;
+    std::byte* current;  // next free byte
 };
 
 }  // namespace utils
