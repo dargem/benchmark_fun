f5a6ba0eac06afc4f90de38384bc1202bf446312
dargem <genericz145@gmail.com>
2026-05-02 23:03:33 +1000

    more arena

--- src/utils/arena.hpp
+++ src/utils/arena.hpp
@@ -2,11 +2,12 @@
 #include <cstddef>
 #include <cstdint>
 #include <cstring>
+#include <new>
 #include <type_traits>
 
 namespace utils {
 
-template <bool Aligned>
+template <bool Aligned = true>
 class Arena {
    public:
     Arena() : begin(new std::byte[CAPACITY]), end(begin + CAPACITY), current(begin) {}
@@ -28,29 +29,67 @@ class Arena {
      * @tparam N the number of objects wanted to hold
      * @return T* to the space
      */
-    template <typename T, size_t N>
-        requires std::same_as<std::remove_cvref_t<T>, T>
+    template <typename T, size_t N = 1>
+        requires std::same_as<std::remove_cvref_t<T>, T> && std::is_trivially_destructible_v<T>
     T* allocate() {
         if constexpr (Aligned) {
             // we need to check we are adding stuff with proper alignment
             // branchless increment to an aligned address, 8 - 13 % 8 gets 3
-            current += (alignof(T) - reinterpret_cast<uintptr_t>(current) % alignof(T));
-            T* ptr = current;
+            current +=
+                (alignof(T) - reinterpret_cast<uintptr_t>(current) % alignof(T)) % alignof(T);
+            T* ptr = reinterpret_cast<T*>(current);
             current += sizeof(T) * N;
             return ptr;
         } else {
-            // just slop it in probably all good!
             T* ptr = current;
             current += sizeof(T) * N;
             return ptr;
         }
     }
 
-    template <typename T, typename... Ts>
-    T* create(Ts... args) {
-        current += (alignof(T) - reinterpret_cast<uintptr_t>(current) % alignof(T));
-        T* ptr = new (current) T(args...);
-        current += sizeof(T);
+    /**
+     * @brief Allocates space for N objects with sizeof(T)
+     *
+     * @tparam T the type of the object used in this space
+     * @tparam N the number of objects wanted to hold
+     * @return T* to the space
+     */
+    template <typename T>
+        requires std::same_as<std::remove_cvref_t<T>, T> && std::is_trivially_destructible_v<T>
+    T* allocate(size_t n) {
+        if constexpr (Aligned) {
+            // we need to check we are adding stuff with proper alignment
+            // branchless increment to an aligned address, 8 - 13 % 8 gets 3
+            current +=
+                (alignof(T) - reinterpret_cast<uintptr_t>(current) % alignof(T)) % alignof(T);
+            T* ptr = reinterpret_cast<T*>(current);
+            current += sizeof(T) * n;
+            return ptr;
+        } else {
+            T* ptr = current;
+            current += sizeof(T) * n;
+            return ptr;
+        }
+    }
+
+    template <typename T, typename... Args>
+        requires std::same_as<std::remove_cvref_t<T>, T> && std::is_trivially_destructible_v<T>
+    T* emplace(Args&&... args) {
+        if constexpr (Aligned) {
+            // we need to check we are adding stuff with proper alignment
+            // branchless increment to an aligned address, 8 - 13 % 8 gets 3
+            current +=
+                (alignof(T) - reinterpret_cast<uintptr_t>(current) % alignof(T)) % alignof(T);
+
+            T* ptr = new (current) T(args...);
+            current += sizeof(T);
+
+            return ptr;
+        } else {
+            T* ptr = new (current) T(args...);
+            current += sizeof(T);
+            return ptr;
+        }
     }
 
     void reset() { current = begin; }
