f09b2d1dbd75f022a2876a70637a8b03fee260f8
dargem <genericz145@gmail.com>
2026-06-15 19:26:00 +1000

    start localities and some other stuff

--- src/benchmarks/localities/data_localities.hpp
+++ src/benchmarks/localities/data_localities.hpp
@@ -5,12 +5,13 @@
 
 #include "src/benchmarks/benchable.hpp"
 #include "src/utils/xoroshiro_64_star.hpp"
+#include "utils/aligned_allocator.hpp"
 
 namespace benchmarks {
 
 enum class LocalityType { Data, Instruction };
 
-template <LocalityType T>
+template <LocalityType T, size_t LOOP_UNROLL>
 class Localities : public Benchable {
    private:
     static constexpr std::string type = []() {
@@ -21,16 +22,52 @@ class Localities : public Benchable {
 
    public:
     Localities(size_t numElements) : Benchable(std::format("{} locality benchmark", type)) {
-        vec.resize(numElements);
         XoroshiroRNG rng{};
+        health.resize(numElements);
+        defense.resize(numElements);
+        incomingDamage.resize(numElements);
+        attack.resize(numElements);
+        x_pos.resize(numElements);
+        y_pos.resize(numElements);
+        z_pos.resize(numElements);
+
+        rng.fill_aligned_uint32(health.data(), numElements);
+        rng.fill_aligned_uint32(defense.data(), numElements);
+        rng.fill_aligned_uint32(incomingDamage.data(), numElements);
+        rng.fill_aligned_uint32(attack.data(), numElements);
+        rng.fill_aligned_float(x_pos.data(), numElements);
+        rng.fill_aligned_float(y_pos.data(), numElements);
+        rng.fill_aligned_float(z_pos.data(), numElements);
     }
 
-    void runBenchmark(size_t iterations) override {}
+    void runBenchmark(size_t iterations) override {
+        auto applyDamage = [](uint32_t& health, uint32_t defense, uint32_t incomingDamage) {
+            health -= (incomingDamage / defense);
+        };
+
+        auto
+
+            for (size_t i{}; i < iterations; ++i) {
+            // Should just use 1 iteration
+            if constexpr (T == LocalityType::Instruction) {
+                // We loop through each system
+            }
+        }
+    }
 
     void resetBenchmark() override {}
 
    private:
-    std::vector<float> vec;
+    std::vector<int, utils::AlignedAllocator<int, XoroshiroRNG::REGISTER_BYTE_SIZE>> health;
+    std::vector<uint32_t, utils::AlignedAllocator<uint32_t, XoroshiroRNG::REGISTER_BYTE_SIZE>>
+        defense;
+    std::vector<uint32_t, utils::AlignedAllocator<uint32_t, XoroshiroRNG::REGISTER_BYTE_SIZE>>
+        incomingDamage;
+    std::vector<uint32_t, utils::AlignedAllocator<uint32_t, XoroshiroRNG::REGISTER_BYTE_SIZE>>
+        attack;
+    std::vector<float, utils::AlignedAllocator<float, XoroshiroRNG::REGISTER_BYTE_SIZE>> x_pos;
+    std::vector<float, utils::AlignedAllocator<float, XoroshiroRNG::REGISTER_BYTE_SIZE>> y_pos;
+    std::vector<float, utils::AlignedAllocator<float, XoroshiroRNG::REGISTER_BYTE_SIZE>> z_pos;
 };
 
 }  // namespace benchmarks
---
+++ src/utils/aligned_allocator.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <stdlib.h>
+
+#include <new>
+
+namespace utils {
+
+template <typename E, std::size_t BYTE_ALIGN = std::hardware_destructive_interference_size>
+class AlignedAllocator {
+    static_assert(alignof(E) <= BYTE_ALIGN, "Aligned to a value smaller than element");
+
+   public:
+    using value_type = E;
+    static constexpr std::align_val_t ALIGNMENT{BYTE_ALIGN};
+
+    AlignedAllocator() noexcept {};
+
+    template <typename T>
+    AlignedAllocator(const AlignedAllocator<T, BYTE_ALIGN>&) noexcept {}
+
+    template <typename T>
+    inline bool operator==(const AlignedAllocator<T, BYTE_ALIGN>& other) const noexcept {
+        return true;
+    }
+
+    template <typename T>
+    inline bool operator!=(const AlignedAllocator<T, BYTE_ALIGN>& other) const noexcept {
+        return false;
+    }
+
+    inline value_type* allocate(const std::size_t n) const {
+        value_type* ptr;
+        auto ret = posix_memalign((void**)&ptr, BYTE_ALIGN, sizeof(E) * n);
+        if (ret != 0) {
+            throw std::bad_alloc();
+        }
+        return ptr;
+    }
+
+    // We need to implement a rebind as our Allocator isn't only templated by type
+    // So we need to explicitly tell it how to rebind the allocator to a different type
+    template <typename U>
+    struct rebind {
+        using other = AlignedAllocator<U, BYTE_ALIGN>;
+    };
+
+    inline void deallocate(value_type* ptr, std::size_t n) const noexcept { free(ptr); }
+};
+
+}  // namespace utils
